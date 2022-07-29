741bda92dbfe0aa9335a1ea721b29939e285b258
Laurian Gridinoc <laurian@gridinoc.name>
2022-07-29 20:45:59 +0300

    amplify delete

--- amplify/backend/auth/OpenEditor/override.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-param-reassign */
-/* eslint-disable @typescript-eslint/explicit-function-return-type */
-import { AmplifyAuthCognitoStackTemplate } from '@aws-amplify/cli-extensibility-helper';
-
-export function override(resources: AmplifyAuthCognitoStackTemplate) {
-  resources.userPool.adminCreateUserConfig = {
-    allowAdminCreateUserOnly: true,
-  };
-}
--- amplify/backend/storage/s3openeditorstorage0387b458/override.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-param-reassign */
-/* eslint-disable @typescript-eslint/explicit-function-return-type */
-import { AmplifyS3ResourceTemplate } from '@aws-amplify/cli-extensibility-helper';
-
-export function override(resources: AmplifyS3ResourceTemplate) {
-  resources.s3Bucket.versioningConfiguration = {
-    status: 'Enabled',
-  };
-}
