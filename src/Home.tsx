284beda7f2af6ff6b3cfdefa2e5ac9179aa02d10
Laurian Gridinoc <laurian@gridinoc.name>
2022-08-01 05:32:43 +0300

    transcript status, upload WIP

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useCallback } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { DataStore } from 'aws-amplify';
 import { Layout, PageHeader, Button } from 'antd';
 
-import { User } from './models';
+import { User, Transcript } from './models';
 
 const { Header, Content } = Layout;
 
@@ -13,15 +14,108 @@ interface HomeProps {
 }
 
 const Home = ({ user, groups, userMenu }: HomeProps): JSX.Element => {
-  console.log('Home');
+  const history = useHistory();
+
+  const newTranscript = useCallback(async () => {
+    const transcript = await DataStore.save(
+      new Transcript({
+        title: 'test',
+        language: 'en-US',
+        media: '{}',
+        metadata: '{}',
+        status: JSON.stringify({
+          step: 0,
+          steps: [
+            {
+              type: 'upload',
+              progress: 0,
+              status: 'wait',
+              title: {
+                default: 'Upload',
+                wait: 'Ready to upload',
+                process: 'Uploading',
+                finish: 'Uploaded',
+                error: 'Upload failed',
+              },
+              description: {
+                default: 'Upload a media file',
+              },
+            },
+            {
+              type: 'transcode',
+              progress: 0,
+              // status: 'wait',
+              title: {
+                default: 'Transcode',
+                wait: 'Ready to transcode',
+                process: 'Transcoding',
+                finish: 'Transcoded',
+                error: 'Transcode failed',
+              },
+              description: {
+                default: 'Transcode to a speech-recognition format',
+              },
+            },
+            {
+              type: 'transcribe',
+              progress: 0,
+              // status: 'wait',
+              title: {
+                default: 'Transcribe',
+                wait: 'Ready to transcribe',
+                process: 'Transcribing',
+                finish: 'Transcribed',
+                error: 'Transcribe failed',
+              },
+              description: {
+                default: 'Transcribe the speech-recognition format',
+              },
+            },
+            {
+              type: 'edit',
+              progress: 0,
+              // status: 'wait',
+              title: {
+                default: 'Edit',
+                wait: 'Ready to edit',
+                process: 'Editing',
+                finish: 'Edited',
+                error: 'Edit failed',
+              },
+              description: {
+                default: 'Edit the transcript',
+              },
+            },
+            {
+              type: 'align',
+              progress: 0,
+              // status: 'wait',
+              title: {
+                default: 'Realign',
+                wait: 'Ready to realign',
+                process: 'Realigning',
+                finish: 'Realigned',
+                error: 'Realign failed',
+              },
+              description: {
+                default: 'Realign the transcript',
+              },
+            },
+          ],
+        }),
+      }),
+    );
+
+    console.log({ transcript });
+    history.push(`/${transcript.id}`);
+  }, [history]);
 
   return (
     <Layout>
       <PageHeader title="OpenEditor" extra={userMenu} />
       <Content>
-        <Link to="/new">
-          <Button>New Transcript</Button>
-        </Link>
+        <Button onClick={newTranscript}>New Transcript</Button>
+        <pre>{JSON.stringify(user, null, 2)}</pre>
       </Content>
     </Layout>
   );
--- src/Transcript.tsx
+++ src/Transcript.tsx
@@ -1,18 +1,21 @@
 import React, { useState, useEffect, useMemo, useCallback } from 'react';
 import { useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
-import { Storage } from 'aws-amplify';
+import { DataStore, Storage } from 'aws-amplify';
 import { v4 as uuidv4 } from 'uuid';
-import { Layout, Col, Row, PageHeader, Card, Steps, Button, Upload } from 'antd';
+import { Layout, Col, Row, PageHeader, Card, Steps, Button, Upload, Select, Space } from 'antd';
 import { UploadOutlined } from '@ant-design/icons';
 import { RcFile } from 'antd/lib/upload';
 
 import { User, Transcript } from './models';
+import languages from './data/aws-transcribe-languages.json';
 
-import type { UploadRequestOption } from 'rc-upload/lib/interface';
+import type { UploadRequestOption, UploadProgressEvent, UploadRequestError } from 'rc-upload/lib/interface';
+import type { UploadFile, UploadChangeParam } from 'antd/lib/upload/interface';
 
 const { Content } = Layout;
 const { Step } = Steps;
+const { Option } = Select;
 
 interface TranscriptPageProps {
   user: User | undefined;
@@ -25,75 +28,175 @@ const TranscriptPage = ({ user, groups, transcripts, userMenu }: TranscriptPageP
   const params = useParams();
   const { uuid } = params as Record<string, string>;
 
+  const [transcript, setTranscript] = useState<Transcript | undefined>(undefined);
+  useEffect(() => setTranscript(transcripts?.find(({ id }) => id === uuid)), [transcripts, uuid]);
+
+  const [steps, setSteps] = useState([]);
   const [step, setStep] = useState(0);
   const [status, setStatus] = useState<'wait' | 'process' | 'finish' | 'error' | undefined>('wait');
   const [progress, setProgress] = useState(0);
 
-  // const transcript = useMemo(
-  //   () => transcripts?.find(({ id }) => id === `${params.a}-${params.b}-${params.c}-${params.d}-${params.e}`),
-  //   [params, transcripts],
-  // );
+  const [fileList, setFileList] = useState<UploadFile[]>([
+    {
+      uid: '-1',
+      name: 'xxx.png',
+      status: 'done',
+      url: 'http://www.baidu.com/xxx.png',
+    },
+  ]);
 
-  // console.log({ params, user, transcripts });
+  useEffect(() => {
+    if (!transcript) return;
 
-  const handleUpload = useCallback(({ file, onError, onSuccess }: UploadRequestOption) => {
-    setStatus('process');
-    console.log('handleUpload', file);
+    const { step, steps } = (transcript.status as unknown as Record<string, any>) ?? { step: 0, steps: [] };
 
-    const { name, type, size } = file as RcFile;
+    setSteps(steps);
+    setStep(step);
+    setStatus(steps[step].status);
+    setProgress(steps[step].progress);
+  }, [transcript]);
 
-    const key = `uploads/${new Date().toISOString().split(/[-|T]/).join('/')}/${uuidv4()}.${name.split('.').pop()}`;
+  const updateStatus = useCallback(
+    async ({
+      step,
+      status,
+      progress,
+    }: {
+      step: number;
+      status: 'wait' | 'process' | 'finish' | 'error' | undefined;
+      progress: number;
+    }) => {
+      const original = await DataStore.query(Transcript, uuid);
+      if (!original) return;
+      await DataStore.save(
+        Transcript.copyOf(original, updated => {
+          const originalStatus = JSON.parse(JSON.stringify(original.status));
+          // eslint-disable-next-line no-param-reassign
+          updated.status = JSON.stringify({
+            ...originalStatus,
+            steps: [
+              ...originalStatus.steps.slice(0, step),
+              {
+                ...originalStatus.steps[step],
+                status,
+                progress,
+              },
+              ...originalStatus.steps.slice(step + 1),
+            ],
+          });
+        }),
+      );
+    },
+    [uuid],
+  );
 
-    Storage.put(key, file, {
-      progressCallback: ({ loaded, total }) => setProgress((100 * loaded) / total),
-    })
-      .then(async ({ key }) => {
-        // this.setState({ uploadAlert: null });
+  const handleUpload = useCallback(
+    async ({ file, onError, onSuccess, onProgress, filename, data }: UploadRequestOption) => {
+      console.log({ file, filename, data });
+      setStatus('process');
+      updateStatus({ step: 0, status: 'process', progress: 0 });
+
+      const { name, type, size } = file as RcFile;
+
+      const key = `uploads/${new Date().toISOString().split(/[-|T]/).join('/')}/${uuidv4()}.${name.split('.').pop()}`;
+
+      try {
+        await Storage.put(key, file, {
+          progressCallback: ({ loaded, total }) => {
+            const percent = Math.floor((loaded / total) * 100);
+            setProgress(percent);
+            if (onProgress) onProgress({ percent } as UploadProgressEvent);
+          },
+        });
         console.log({ key });
-        setStatus('finish');
-        setStep(1);
-        setProgress(0);
-        setStatus('wait');
-      })
-      .catch(err => {
-        console.log(err);
-        // this.setState({ uploadAlert: err });
+      } catch (error) {
+        console.log(error);
         setStatus('error');
-      });
+        updateStatus({ step: 0, status: 'error', progress: 0 });
+        if (onError) onError(error as UploadRequestError); // TBD
+      }
+      setStatus('finish');
+      setProgress(0);
+      updateStatus({ step: 0, status: 'finish', progress: 100 });
+      if (onSuccess) onSuccess(null); // TBD
+    },
+    [setProgress, setStatus, updateStatus],
+  );
+
+  const handleRemove = useCallback((file: UploadFile) => {
+    console.log(file);
+    // TODO: set status to wait, update transcript again
   }, []);
+
+  const handleUploadChange = useCallback((change: UploadChangeParam) => {
+    console.log(change);
+    // TODO: set status to wait, update transcript again?
+  }, []);
+
+  const [language, setLanguage] = useState<string>('en-US');
+  const languageOptions = useMemo(
+    () =>
+      languages.map(language => ({
+        label: `${language.Language} [${language['Language Code']}]`,
+        value: language['Language Code'],
+      })),
+    [],
+  );
 
   return (
     <Layout>
-      <PageHeader
-        className="site-page-header"
-        title={uuid}
-        // subTitle="This is a subtitle"
-        extra={userMenu}
-      />
-      <Content style={{ minHeight: '100vh' }}>
-        <Row>
-          <Col span={12} offset={6}>
-            <Card title="Operations in progress">
-              <Steps current={step} direction="vertical" percent={progress} status={status}>
-                <Step
-                  title="Upload"
-                  description={
-                    <Upload showUploadList={false} maxCount={1} customRequest={handleUpload}>
-                      <Button
-                        icon={<UploadOutlined />}
-                        disabled={step !== 0 || status === 'process' || status === 'finish'}>
-                        Upload
-                      </Button>
-                    </Upload>
-                  }
-                />
-                <Step title="Transcode" description="This is a description." />
-                <Step title="Transcribe" description="This is a description." />
-              </Steps>
-            </Card>
-          </Col>
-        </Row>
-      </Content>
+      <PageHeader className="site-page-header" title={transcript?.title ?? uuid} subTitle={uuid} extra={userMenu} />
+      {transcript ? (
+        <Content style={{ minHeight: '100vh' }}>
+          <Select defaultValue={language} style={{ width: 230 }} onChange={setLanguage}>
+            {languageOptions.map(({ label, value }) => (
+              <Option key={value} value={value}>
+                {label}
+              </Option>
+            ))}
+          </Select>
+          <Row>
+            <Col span={12} offset={6}>
+              <Card title="Operations in progress">
+                <Steps current={step} direction="vertical" percent={progress} status={status}>
+                  {steps.map(({ type, status, title, description }, index) =>
+                    type === 'upload' ? (
+                      <Step
+                        key={type}
+                        title={title[status ?? 'default']}
+                        description={
+                          <Upload
+                            fileList={fileList} // TODO: update file list from status/data, and onChange, fix download
+                            showUploadList={{ showRemoveIcon: true, showDownloadIcon: true }}
+                            maxCount={1}
+                            customRequest={handleUpload}
+                            onRemove={handleRemove}
+                            onChange={handleUploadChange}
+                            onDownload={console.log}>
+                            <Button
+                              icon={<UploadOutlined />}
+                              disabled={step !== index || status === 'process' || status === 'finish'}>
+                              Upload
+                            </Button>
+                          </Upload>
+                        }
+                      />
+                    ) : (
+                      <Step
+                        key={type}
+                        title={title[status ?? 'default']}
+                        description={description?.[status ?? 'default'] ?? description?.[null ?? 'default']}
+                      />
+                    ),
+                  )}
+                </Steps>
+              </Card>
+            </Col>
+          </Row>
+        </Content>
+      ) : (
+        'Loading…'
+      )}
     </Layout>
   );
 };
