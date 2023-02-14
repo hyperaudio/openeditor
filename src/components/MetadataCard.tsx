9bca49f8986cc6c14074227f699f5d3a6481678d
Laurian Gridinoc <laurian@gridinoc.name>
2023-02-15 01:01:01 +0200

    edit title, speakers

--- src/components/MetadataCard.tsx
+++ src/components/MetadataCard.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any, jsx-a11y/label-has-associated-control */
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useState, useMemo } from 'react';
+import { DataStore } from 'aws-amplify';
 import { useAtom } from 'jotai';
 import { Card, Input, Space, Button } from 'antd';
 
@@ -18,7 +19,30 @@ const MetadataCard = ({
   speakers: { [key: string]: any };
   setSpeakers: (speakers: { [key: string]: any }) => void;
 }): JSX.Element | null => {
+  const [title, setTitle] = useState(transcript?.title);
   const [debugMode] = useAtom(debugModeAtom);
+
+  const handleTitleChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      const { value: title } = e.target;
+      if (title) setTitle(title);
+    },
+    [setTitle],
+  );
+
+  const updateTitle = useCallback(
+    async (e: any) => {
+      const original = await DataStore.query(Transcript, transcript?.id as string);
+      if (!original) return;
+      await DataStore.save(
+        Transcript.copyOf(original, (updated: any) => {
+          // eslint-disable-next-line no-param-reassign
+          updated.title = title;
+        }),
+      );
+    },
+    [transcript, title],
+  );
 
   return (
     <Card size="small">
@@ -26,18 +50,17 @@ const MetadataCard = ({
         <Space style={{ width: '100%' }} direction="vertical" size="small">
           <span>Title</span>
           <Input.Group compact>
-            <Input style={{ width: 'calc(100% - 100px)' }} value={transcript?.title} />
-            <Button type="primary">Update</Button>
+            <Input style={{ width: 'calc(100% - 100px)' }} value={title} onChange={handleTitleChange} />
+            <Button type="primary" onClick={updateTitle}>
+              Update
+            </Button>
           </Input.Group>
         </Space>
 
         <Space style={{ width: '100%' }} direction="vertical" size="small">
           <span>Speaker names</span>
           {Object.keys(speakers).map(speakerId => (
-            <Input.Group compact>
-              <Input style={{ width: 'calc(100% - 100px)' }} value={speakers[speakerId].name} />
-              <Button type="primary">Update</Button>
-            </Input.Group>
+            <SpeakerNameInput id={speakerId} speakers={speakers} setSpeakers={setSpeakers} />
           ))}
         </Space>
       </Space>
@@ -45,4 +68,40 @@ const MetadataCard = ({
   );
 };
 
+const SpeakerNameInput = ({
+  id,
+  speakers,
+  setSpeakers,
+}: {
+  id: string;
+  speakers: { [key: string]: any };
+  setSpeakers: (speakers: { [key: string]: any }) => void;
+}): JSX.Element => {
+  const speaker = useMemo(() => speakers[id], [speakers, id]);
+  const [name, setName] = useState(speaker.name);
+
+  const handleNameChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      const { value: name } = e.target;
+      if (name) setName(name);
+    },
+    [setName],
+  );
+
+  const updateSpeakerName = useCallback(
+    (e: any) => {
+      setSpeakers({ ...speakers, [id]: { ...speaker, name } });
+    },
+    [id, speaker, name, speakers, setSpeakers],
+  );
+
+  return (
+    <Input.Group compact>
+      <Input style={{ width: 'calc(100% - 100px)' }} value={name} onChange={handleNameChange} />
+      <Button type="primary" onClick={updateSpeakerName}>
+        Update
+      </Button>
+    </Input.Group>
+  );
+};
 export default MetadataCard;
